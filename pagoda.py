ab08a6f054efe2c327d5d3273cd2c111a46ea8fc
Talon Carballeira <taloncarb@gmail.com>
2024-04-10 11:43:59 -0600

    added an add function, not sure if it works though

--- pagoda.py
+++ pagoda.py
@@ -13,6 +13,7 @@ class Pagoda():
  
     
     def merge(self, treeA, treeB):
+            '''
             # base case
             # if node above is the root
             if rc.p.p.value > rc.p.value or lc.p.p.value > lc.p.value:   # only the root node's parent is greater than the node
@@ -25,7 +26,7 @@ class Pagoda():
                     self.root = rc
                     lc.p = rc
                     rc.p = null
-                    set root children?
+                  #  set root children?
             
             # recursive cases    
             elif lc.value > rc.value:
@@ -37,9 +38,46 @@ class Pagoda():
                 parent = rc.p
                 rc.p = lc
                 compare(parent, lc)
-    
+        '''
+            
     def add(self,new_node):
-        pass
+        return compare(self.p,new_node)
+        
+    def compare(self, d, h):  # d is 
+        if h.p.value > h.value: # h is the root
+            if d.value < h.value: # d needs to be the new root
+                temp_child = d.c
+                d.p = h.p # set new root (d) parent to the old root parent(h)  which is the leftmost
+                d.c = h.c  # set new root (d) child to the old root child(h)  which is the rightmost
+                h.p = d
+                h.c = temp_child
+                self.root = d  
+                
+        elif d.p.value > d.value: #  d is the root 
+            temp_child = h.c   # save new root child
+            h.p = d.p # set new root parent to the old root parent which is the leftmost
+            h.c = d.c  # set new root child to the old root child which is the rightmost
+            d.p = h
+            d.c = temp_child
+            self.root = h
+
+        elif d.value < h.value:   # don't need to iterate up
+            h.p = d  # set h parent
+            if self.root.p == d: 
+                self.root.p = h # change root to point to new smallest
+                h.c = h  # set h child to itself
+            elif self.root.c == d:  # check if d is smallest 
+                self.root.c = h # change root to point to new smallest
+                h.c = h  # set h child to itself
+
+        elif d.value > h.value: # h needs to be moved higher up. 
+            h.p = d.p  
+            d.p = h    
+            self.compare(h,h.p)
+
+
+
+
 
     def remove_min(self):
         '''
@@ -72,6 +110,6 @@ class Pagoda():
         pass
         
         
-        
+
 
     
